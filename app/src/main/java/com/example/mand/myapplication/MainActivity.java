4d45bd5360f52abfae4536581e76797533758bc6
maickol12 <rocm960107@outlook.com>
2017-05-11 00:18:35 -0500

    maquetado de la parte principal

--- app/src/main/java/com/example/mand/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mand/myapplication/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.mand.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbarGen);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        toolbar.setTitle("Variedades");
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        return true;
     }
 }
