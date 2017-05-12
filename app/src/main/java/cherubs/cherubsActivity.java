4ef4d94df81f7f18b3c27dbb76012f0f55af5abe
maickol12 <rocm960107@outlook.com>
2017-05-12 00:56:26 -0500

    update cherubs

--- app/src/main/java/cherubs/cherubsActivity.java
+++ app/src/main/java/cherubs/cherubsActivity.java
@@ -3,11 +3,32 @@ package cherubs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.example.mand.myapplication.R;
 
 public class cherubsActivity extends AppCompatActivity{
+    private Toolbar toolbar;
     public void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.activity_cherubs);
+        toolbar = (Toolbar) findViewById(R.id.toolbarGen);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        toolbar.setTitle("Cherubs");
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+            break;
+        }
+        return true;
     }
 }
