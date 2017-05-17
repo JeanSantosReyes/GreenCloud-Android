8c2553ee6d6a18ebf0e66f9d6e5a80a72d0a033a
maickol12 <rocm960107@outlook.com>
2017-05-17 17:13:11 -0500

    maickol

--- app/src/main/java/com/example/mand/myapplication/secciones.java
+++ app/src/main/java/com/example/mand/myapplication/secciones.java
@@ -3,12 +3,16 @@ package com.example.mand.myapplication;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class secciones extends AppCompatActivity {
     private Spinner spinner;
+    private boolean flag = false;
     public void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.secciones);
@@ -21,5 +25,21 @@ public class secciones extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.tomates_array,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new oyenteSpinner());
+    }
+    public class oyenteSpinner implements AdapterView.OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if(flag){
+                Toast.makeText(secciones.this," "+position, Toast.LENGTH_LONG).show();
+            }else{
+                flag = true;
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
     }
 }
