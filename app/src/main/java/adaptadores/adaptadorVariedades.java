16b6a26fb4dca52c18c802dfec17b0f6ad04c71e
maickol12 <rocm960107@outlook.com>
2017-08-10 19:17:19 -0500

    maickol

---
+++ app/src/main/java/adaptadores/adaptadorVariedades.java
@@ -0,0 +1,5 @@
+package adaptadores;
+
+
+public class adaptadorVariedades {
+}
--- app/src/main/java/com/example/mand/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mand/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +14,7 @@ import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
-
+    private RecyclerView recyclerVariedades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //CODIGO DEL RECICLER
+        recyclerVariedades = (RecyclerView) findViewById(R.id.reciclerviewVariedades);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerVariedades.setLayoutManager(llm);
+
 
         if(Build.VERSION.SDK_INT >= 21){
             Window window = this.getWindow();
